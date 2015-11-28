2122bea741caf9d11c5cf8fb841deddc0e66fec8
Remon Lam <remon.lam@lamnet.nl>
2015-11-28 13:17:19 +0100

    added "-y"

--- rpi-rasp-jessie-install-docker.sh
+++ rpi-rasp-jessie-install-docker.sh
@@ -2,7 +2,7 @@
 sudo apt-get update && sudo apt-get upgrade
 
 # Install Docker don't forget the .io part otherwise you get something different ;-)
-sudo apt-get install docker.io
+sudo apt-get install -y docker.io
 
 # Add the PI user account a member of the docker group, so you don't need to use "sudo"
 sudo groupadd docker
